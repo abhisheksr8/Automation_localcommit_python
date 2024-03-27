1dd7c1eeac21c86d11114c4d2b4524e28facc565
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2024-03-27 06:52:37 +0000

    ❇️ New     - Pipeline        - Automation_localcommit_python_Pipeline     - Dataset        - test_dataset

---
+++ pipelines/Automation_localcommit_python_Pipeline/code/automation_localcommit_python_pipeline/pipeline.py
@@ -0,0 +1,33 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from automation_localcommit_python_pipeline.config.ConfigStore import *
+from automation_localcommit_python_pipeline.udfs.UDFs import *
+from prophecy.utils import *
+from automation_localcommit_python_pipeline.graph import *
+
+def pipeline(spark: SparkSession) -> None:
+    df_test_dataset = test_dataset(spark)
+    df_config_column_reformat = config_column_reformat(spark, df_test_dataset)
+
+def main():
+    spark = SparkSession.builder\
+                .config("spark.default.parallelism", "4")\
+                .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
+                .enableHiveSupport()\
+                .appName("Automation_localcommit_python_Pipeline")\
+                .getOrCreate()
+    Utils.initializeFromArgs(spark, parse_args())
+    spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/Automation_localcommit_python_Pipeline")
+    registerUDFs(spark)
+    
+    MetricsCollector.instrument(
+        spark = spark,
+        pipelineId = "pipelines/Automation_localcommit_python_Pipeline",
+        config = Config
+    )(
+        pipeline
+    )
+
+if __name__ == "__main__":
+    main()
