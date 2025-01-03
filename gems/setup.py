671aea6e796d0b216ed2f7231ea0343e130a60ce
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-01-03 08:07:23 +0000

    [Prophecy] Release version 1.0.0

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 packages_to_include = find_packages(exclude = ['test.*', 'test', 'test_manual'])
 setup(
     name = 'abhishekse2etestsprophecyioteam_projectautomationpythonreleaseapi1735891051639',
-    version = '0.0.1',
+    version = '17358910516391.0.0',
     packages = packages_to_include,
     description = '',
     install_requires = [],
