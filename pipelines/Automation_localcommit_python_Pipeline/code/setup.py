b1ed6b679ea6868a1d6b36bc7ef4c0dcdd4426a4
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-03-19 01:25:07 +0000

    Add new pipeline 'Automation_localcommit_python_Pipeline' and dataset 'test_dataset'Simple workflow Commit

---
+++ pipelines/Automation_localcommit_python_Pipeline/code/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'Automation_localcommit_python_Pipeline',
+    version = '1.0',
+    packages = (
+      find_packages(include = ('automation_localcommit_python_pipeline*', ))
+      + ['prophecy_config_instances.automation_localcommit_python_pipeline']
+    ),
+    package_dir = {
+      'prophecy_config_instances.automation_localcommit_python_pipeline': 'configs/resources/automation_localcommit_python_pipeline'
+    },
+    package_data = {'prophecy_config_instances.automation_localcommit_python_pipeline' : ['*.json', '*.py', '*.conf']},
+    description = 'workflow',
+    install_requires = [
+'prophecy-libs==1.9.38'],
+    entry_points = {
+'console_scripts' : [
+'main = automation_localcommit_python_pipeline.pipeline:main'], },
+    data_files = [(".prophecy", [".prophecy/workflow.latest.json"])],
+    extras_require = {
+'test' : ['pytest', 'pytest-html', 'pytest-cov'], }
+)
